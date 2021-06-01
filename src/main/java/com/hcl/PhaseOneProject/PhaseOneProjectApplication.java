9c04c155751928c59ab523e59119ab849e392817
Joseph Byrd <joseph.byrdjr@hcl.com>
2021-06-01 14:48:32 -0400

    Initial commit

---
+++ src/main/java/com/hcl/PhaseOneProject/PhaseOneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.PhaseOneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseOneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseOneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/PhaseOneProject/PhaseOneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.PhaseOneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseOneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
