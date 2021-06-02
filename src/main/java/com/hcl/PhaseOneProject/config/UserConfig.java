918f6d5ed061b9083a0ac86393af69118a84ee56
Braiden King <braiden.king@hcl.com>
2021-06-02 13:16:29 -0700

    Config

---
+++ src/main/java/com/hcl/PhaseOneProject/config/UserConfig.java
@@ -0,0 +1,30 @@
+package com.hcl.PhaseOneProject.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class UserConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
+
+	
+	@Override 
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/")
+		.permitAll().antMatchers("/*").hasAnyRole("USER", "ADMIN")
+		.and().formLogin()
+		.loginPage("/login").permitAll();
+		http.csrf().disable();
+	}
+}
--- src/main/java/com/hcl/PhaseOneProject/controllers/UserController.java
+++ src/main/java/com/hcl/PhaseOneProject/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +20,10 @@ public class UserController {
 	@Autowired
 	UserService userService;
 	
-	
+	@GetMapping("/login")
+	public ModelAndView login() {
+		return new ModelAndView("login");
+	}
 	
 	
 
